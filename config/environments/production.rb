5414c0703ddd0cdad749d3b872ccab935cefad68
Sudhir Jonathan <sudhir.j@gmail.com>
2012-06-28 22:54:13 +0530

    added cache headers for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,10 +7,7 @@ Airport::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
+  
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -44,6 +41,8 @@ Airport::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   config.action_controller.asset_host = "https://d1fv3e3dqowl27.cloudfront.net"
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=31536000"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
