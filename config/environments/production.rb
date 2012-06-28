1cd609bc2b9de94d50d1eed9613a50341685bb49
Sudhir Jonathan <sudhir.j@gmail.com>
2012-06-28 22:47:10 +0530

    added cloudfront asset host and uncommented it out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Airport::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "https://d1fv3e3dqowl27.cloudfront.net"
+  config.action_controller.asset_host = "https://d1fv3e3dqowl27.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
